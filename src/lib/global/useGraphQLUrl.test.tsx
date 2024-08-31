f659337457e6ea55523ca7a10bbb352ce1b005d2
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-31 12:18:55 +0000

    useGraphQLUrlのテストコード追加

---
+++ src/lib/global/useGraphQLUrl.test.tsx
@@ -0,0 +1,60 @@
+import { useGraphQLUrl } from './useGraphQLUrl';
+import { getRestPhpVarName } from './getRestPhpVarName';
+import { render } from '../../jest';
+
+const TEST_ID = '61D2F697';
+
+const Sut: React.FC = () => {
+	const graphqlUrl = useGraphQLUrl();
+	return (
+		<>
+			<p data-testid={ TEST_ID }>{ String( graphqlUrl ) }</p>
+		</>
+	);
+};
+
+const setGlobalVar = ( graphqlUrl: string, wpRestgraphqlUrl: string ) => {
+	const varName = getRestPhpVarName();
+	( global as any )[ varName ] = {
+		graphqlUrl,
+		wpRestgraphqlUrl,
+	};
+};
+
+describe( '[975F9282] useGraphQLUrl', () => {
+	const cleanup = () => {
+		( global as any )[ getRestPhpVarName() ] = undefined;
+	};
+	beforeEach( cleanup );
+	afterEach( cleanup );
+
+	/**
+	 * GraphQLのURLが存在する場合のテスト
+	 */
+	it( '[BE9A4991] useGraphQLUrl - graphqlUrl exists', () => {
+		// ARRANGE
+		setGlobalVar( 'https://example.com/graphql', 'abcde01234' );
+
+		// ACT
+		const { getByTestId } = render( <Sut /> );
+
+		// ASSERT
+		const graphqlUrl = getByTestId( TEST_ID ).textContent;
+		expect( graphqlUrl ).toBe( 'https://example.com/graphql' );
+	} );
+
+	/**
+	 * GraphQLのURLが存在しない場合のテスト
+	 */
+	it( '[57BEB900] useGraphQLUrl - graphqlUrl does not exist', () => {
+		// ARRANGE
+		// Do nothing
+
+		// ACT
+		const { getByTestId } = render( <Sut /> );
+
+		// ASSERT
+		const graphqlUrl = getByTestId( TEST_ID ).textContent;
+		expect( graphqlUrl ).toBe( 'null' ); // 値が存在しない場合はnullが返る
+	} );
+} );
